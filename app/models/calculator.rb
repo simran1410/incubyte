954513cce0b49f91eb0808026dc61cacdecfefd6
simranjeet <simranbains1410@gmail.com>
2024-04-24 16:17:33 +0530

    3rd question allow the add method to handle new lines between no

--- app/models/calculator.rb
+++ app/models/calculator.rb
@@ -2,7 +2,12 @@ class Calculator < ApplicationRecord
   # 1st Question
   def self.add(num)
     return 0 if num.blank?
-    num.split(",").map(&:to_i).sum
+
+    # num.split(",").map(&:to_i).sum
+
+    # 3rd Question
+    # Split the input string by both commas and new lines
+    num.split(/[\n,]/).map(&:to_i).sum
   end
 
   # 2nd Question
@@ -11,5 +16,8 @@ class Calculator < ApplicationRecord
   # Calculator.add("1")     o/p: 1
   # Calculator.add("3,4")   o/p: 7
   # Calculator.add("3,4,4") o/p: 11
+
+  # 3rd Question result
+  # Calculator.add("1\n4,3") o/p: 8
 
 end
