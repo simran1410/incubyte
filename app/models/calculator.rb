b31c0f42818ab10c20d9e66093fa98afb5130c3d
simranjeet <simranbains1410@gmail.com>
2024-04-24 16:23:24 +0530

    4th Question allow different delimiter

--- app/models/calculator.rb
+++ app/models/calculator.rb
@@ -7,7 +7,16 @@ class Calculator < ApplicationRecord
 
     # 3rd Question
     # Split the input string by both commas and new lines
-    num.split(/[\n,]/).map(&:to_i).sum
+    # num.split(/[\n,]/).map(&:to_i).sum
+
+    # 4th Question
+    delimiter = ","
+    if num.start_with?("//")
+      delimiter = num[2]
+      num = num.split("\n", 2)[1]
+    end
+
+    num.split(/[\n#{delimiter}]/).map(&:to_i).sum
   end
 
   # 2nd Question
@@ -20,4 +29,6 @@ class Calculator < ApplicationRecord
   # 3rd Question result
   # Calculator.add("1\n4,3") o/p: 8
 
+  # 4th Question
+  # Calculator.add("//;\n1;2")    o/p: 3
 end
