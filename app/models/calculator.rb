6237a121ae7e4a28c911602ccd03c171b7d6a3b8
simranjeet <simranbains1410@gmail.com>
2024-04-24 16:13:42 +0530

    2nd Question allow the add method to handle any amount of numbers

--- app/models/calculator.rb
+++ app/models/calculator.rb
@@ -1,6 +1,15 @@
 class Calculator < ApplicationRecord
+  # 1st Question
   def self.add(num)
     return 0 if num.blank?
     num.split(",").map(&:to_i).sum
   end
+
+  # 2nd Question
+  # you can test this by using rails console
+  # Calculator.add("")      o/p: 0
+  # Calculator.add("1")     o/p: 1
+  # Calculator.add("3,4")   o/p: 7
+  # Calculator.add("3,4,4") o/p: 11
+
 end
