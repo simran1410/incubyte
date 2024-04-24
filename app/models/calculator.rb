05b2abd10601e3df63f2545c4231a80b2363b7ba
simranjeet <simranbains1410@gmail.com>
2024-04-24 17:31:46 +0530

    updated test cases and 9th Question make sure you can also handle multiple delimiters with length longer than one char with all question separate answers

--- app/models/calculator.rb
+++ app/models/calculator.rb
@@ -1,60 +1,164 @@
 class Calculator < ApplicationRecord
-  # 1st Question
-  def self.add(num)
-    return 0 if num.blank?
+  # 1st Question Create a simple String calculator with a method signature
 
-    # num.split(",").map(&:to_i).sum
+  # def self.add(num)
+  #   return 0 if num.blank?
 
-    # 3rd Question
-    # Split the input string by both commas and new lines
-    # num.split(/[\n,]/).map(&:to_i).sum
+  #   num.split(",").map(&:to_i).sum
+  # end
 
-    # 4th Question
-    delimiter = ","
-    if num.start_with?("//")
-      # delimiter = num[2]
-      delimiters = num.match(/\/\/\[(.+?)\]\n/)
-      delimiter = delimiters[1] if delimiters
-      num = num.split("\n", 2)[1]
-    end
+  #2nd Question Allow the Add method to handle an unknown amount of numbers
 
-    # num_arr = num.split(/[\n#{delimiter}]/).map(&:to_i)
-
-    # 7th Question
-    num_arr = num.split(/[\n#{Regexp.escape(delimiter)}]/).map(&:to_i)
-
-    # 5th Question
-    negative_num = num_arr.select { |n| n < 0 }
-
-    if negative_num.any?
-      raise "negative numbers not allowed #{negative_num.join(",")}"
-    end
-
-    # 6th Question
-    num_arr.reject! { |num| num > 1000 }
-
-    num_arr.sum
-  end
-
-  # 2nd Question
   # you can test this by using rails console
   # Calculator.add("")      o/p: 0
   # Calculator.add("1")     o/p: 1
   # Calculator.add("3,4")   o/p: 7
   # Calculator.add("3,4,4") o/p: 11
 
+  # 3rd Question Allow the Add method to handle new lines between numbers
+
+  # def self.add(num)
+  #   return 0 if num.blank?
+
+  #   # Split the input string by both commas and new lines
+  #   num.split(/[\n,]/).map(&:to_i).sum
+  # end
+
   # 3rd Question result
   # Calculator.add("1\n4,3") o/p: 8
 
-  # 4th Question
+  # 4th Question Support different delimiters
+
+  # def self.add(num)
+  #   return 0 if num.blank?
+
+  #   delimiter = ","
+  #   if num.start_with?("//")
+  #     delimiter = num[2]
+  #     num = num.split("\n", 2)[1]
+  #   end
+
+  #   num.split(/[\n#{delimiter}]/).map(&:to_i).sum
+  # end
+
+  # 4th Question result
   # Calculator.add("//;\n1;2")    o/p: 3
 
-  #5th Question
+  # 5th Question Calling add with a negative number will throw an exception
+
+  # def self.add(num)
+  #   return 0 if num.blank?
+
+  #   delimiter = ","
+  #   if num.start_with?("//")
+  #     delimiter = num[2]
+  #     num = num.split("\n", 2)[1]
+  #   end
+
+  #   num_arr = num.split(/[\n#{delimiter}]/).map(&:to_i)
+  #   negative_num = num_arr.select { |num| num < 0 }
+
+  #   if negative_num.any?
+  #     raise "negative numbers not allowed #{negative_num.join(",")}"
+  #   end
+
+  #   num_arr.sum
+  # end
+
+  # 5th Question result
   # Calculator.add("-1,1,-8")    o/p: RuntimeError (negative numbers not allowed -1,-8)
 
-  #6th Question
+  # 6th Question Numbers bigger than 1000 should be ignored, so adding 2 + 1001 = 2
+
+  # def self.add(num)
+  #   return 0 if num.blank?
+
+  #   delimiter = ","
+  #   if num.start_with?("//")
+  #     delimiter = num[2]
+  #     num = num.split("\n", 2)[1]
+  #   end
+
+  #   numbers_arr = num.split(/[\n#{delimiter}]/).map(&:to_i)
+
+  #   # Ignore numbers larger than 1000
+  #   numbers_arr.reject! { |num| num > 1000 }
+
+  #   numbers_arr.sum
+  # end
+
+  # 6th Question result
   # Calculator.add("2,1002")   o/p: 2
 
-  # 7th Question
+
+  # 7th Question Delimiters can be of any length
+
+  # def self.add(num)
+  #   return 0 if num.blank?
+
+  #   delimiter = ","
+  #   if num.start_with?("//")
+  #     delimiters = num.match(/\/\/\[(.+?)\]\n/)
+  #     delimiter = delimiters[1] if delimiters
+  #     num = num.split("\n", 2)[1]
+  #   end
+
+  #   numbers_arr = num.split(/[\n#{Regexp.escape(delimiter)}]/).map(&:to_i)
+  #   numbers_arr.reject! { |num| num > 1000 } # Ignore numbers larger than 1000
+  #   numbers_arr.sum
+  # end
+
+  # 7th Question result
   # Calculator.add("//[***]\n1***2***3")  o/p: 6
+
+  # 8th Question Allow multiple delimiters
+
+  # def self.add(numbers)
+  #   return 0 if numbers.blank?
+
+  #   delimiters = []
+  #   if numbers.start_with?("//")
+  #     delimiters = numbers.scan(/\[(.*?)\]/).flatten
+  #     numbers = numbers.split("\n", 2)[1]
+  #   end
+
+  #   delimiters_pattern = delimiters.map { |delimiter| Regexp.escape(delimiter) }.join("|")
+  #   numbers_arr = numbers.split(/[\n#{delimiters_pattern}]/).map(&:to_i)
+
+  #   negative_num = numbers_arr.select { |num| num < 0 }
+
+  #   if negative_num.any?
+  #     raise "negative numbers not allowed #{negative_num.join(",")}"
+  #   end
+
+  #   numbers_arr.reject! { |num| num > 1000 }
+
+  #   numbers_arr.sum
+  # end
+
+  # 8th Question result
+  # Calculator.add("//[;][#][$]\n1;2#3$4") o/p: 10
+
+  # 9th Question make sure you can also handle multiple delimiters with length longer than one char
+
+  def self.add(num)
+    return 0 if num.blank?
+
+    delimiters = []
+    if num.start_with?("//")
+      delimiters = num.scan(/\[(.*?)\]/).flatten
+      num = num.split("\n", 2)[1]
+    end
+
+    delimiters_pattern = delimiters.map { |delimiter| Regexp.escape(delimiter) }.join("|")
+    numbers_arr = num.split(/[\n#{delimiters_pattern}]/).map(&:to_i)
+
+    numbers_arr.sum
+  end
+
+  # 9th Question result
+
+  # Calculator.add("//[;;;][###][$]\n1;;;2###3$4") o/p: 10
+
+
 end
--- test/models/calculator_test.rb
+++ test/models/calculator_test.rb
@@ -4,13 +4,17 @@ class CalculatorTest < ActiveSupport::TestCase
   test "add method should return correct sum" do
     assert_equal 0, Calculator.add("")
     assert_equal 1, Calculator.add("1")
+    # assert_equal 6, Calculator.add("1\n2,3")
     # assert_equal 3, Calculator.add("//;\n1;2")
-    assert_equal 6, Calculator.add("1,2,3")
+    # assert_equal 6, Calculator.add("1,2,3")
+    # assert_equal 6, Calculator.add("//[***]\n1***2***3")
     assert_equal 2, Calculator.add("2,1001")
+    assert_equal 10, Calculator.add("//[;][#][$]\n1;2#3$4")
+    assert_equal 10, Calculator.add("//[;;;][###][$]\n1;;;2###3$4")
   end
 
   test "add method should raise exception for negative numbers" do
-    assert_raises(RuntimeError) { Calculator.add("1,-2,3") }
+    # assert_raises(RuntimeError) { Calculator.add("1,-2,3") }
     # assert_raises(RuntimeError) { Calculator.add("//|\n1|2|-3") }
   end
 
